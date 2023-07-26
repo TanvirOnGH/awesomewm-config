3fd5399fdde89f45cb3cea98b6394d49deaa0ff4
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-07-27 03:45:00 +0600

    remove all environment, script and app launches from startup

--- color/blue/autostart-config.lua
+++ color/blue/autostart-config.lua
@@ -12,24 +12,10 @@ local autostart = {}
 -- Application list function
 --------------------------------------------------------------------------------
 function autostart.run()
-	-- environment
-	awful.spawn.with_shell("python ~/scripts/env/pa-setup.py")
-	awful.spawn.with_shell("python ~/scripts/env/color-profile-setup.py")
-	awful.spawn.with_shell("python ~/scripts/env/kbd-setup.py")
-
-	-- gnome environment
-	awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
-
-	-- firefox sync
-	awful.spawn.with_shell("python ~/scripts/firefox/ff-sync.py")
-
 	-- utils
 	awful.spawn.with_shell("picom")
 
 	-- apps
-	awful.spawn.with_shell("gpaste-client start")
-	awful.spawn.with_shell("transmission-gtk -m")
-	awful.spawn.with_shell("pragha --toggle_view")
 end
 
 -- Read and commands from file and spawn them
