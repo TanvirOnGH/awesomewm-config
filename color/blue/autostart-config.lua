a507da794ac4b3e83e4c318efc8d977eff987dac
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-07-28 17:35:56 +0600

    add xset command to speed up typing in startup and fix tabs

--- color/blue/autostart-config.lua
+++ color/blue/autostart-config.lua
@@ -12,10 +12,11 @@ local autostart = {}
 -- Application list function
 --------------------------------------------------------------------------------
 function autostart.run()
-    -- essentials
-    awful.spawn.with_shell("xrandr --output HDMI-0 --primary --mode 1920x1080 --rate 75 --pos 0x0 --rotate normal") -- display output
-    awful.spawn.with_shell("xset -dpms") -- prevent X server from turning off the display
+	-- essentials
+	awful.spawn.with_shell("xrandr --output HDMI-0 --primary --mode 1920x1080 --rate 75 --pos 0x0 --rotate normal") -- display output
+	awful.spawn.with_shell("xset -dpms") -- prevent X server from turning off the display
 	awful.spawn.with_shell("xset s off") -- disable X screen saver feature
+	awful.spawn.with_shell("xset r rate 250 50") -- speed up keyboard auto-repeat rate
 
 	-- utils
 	awful.spawn.with_shell("picom") -- compositor
