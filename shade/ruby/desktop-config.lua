95f57537129c4a9f972e128ef0e59664a038fffb
Tanvir <tanvir.ahmed.tonoy@proton.me>
2023-07-28 13:27:50 +0600

    set calendar widget upper margin size to 50
    Signed-off-by: tanvir.ahmed.tonoy@proton.me

--- shade/ruby/desktop-config.lua
+++ shade/ruby/desktop-config.lua
@@ -226,7 +226,7 @@ function desktop:init(args)
 	-- Calendar
 	--------------------------------------------------------------------------------
 	local cwidth = 100 -- calendar widget width
-	local cy = 21      -- calendar widget upper margin
+	local cy = 50      -- calendar widget upper margin
 	local cheight = wa.height - 2*cy
 
 	local calendar = {
