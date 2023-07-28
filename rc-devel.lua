193817bada860d7dccb40bbc3d3f9a21f07ee97e
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-07-28 15:52:10 +0600

    remove 4th argument for disks module of orange theme

--- rc-devel.lua
+++ rc-devel.lua
@@ -11,14 +11,13 @@ lock.autostart = true
 -- Configuration file selection
 --------------------------------------------------------------------------------
 --local rc = "colorless.rc-colorless"
-
 --local rc = "color.red.rc-red"
-local rc = "color.blue.rc-blue"
+--local rc = "color.blue.rc-blue"
 --local rc = "color.orange.rc-orange"
 --local rc = "color.green.rc-green"
-
---local rc = "shade.ruby.rc-ruby"
 --local rc = "shade.steel.rc-steel"
+
+local rc = "shade.ruby.rc-ruby"
 
 require(rc)
 
