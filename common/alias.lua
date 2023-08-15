bd57ca81c87564867aea0d06c2a0c84cc781b7d9
Tanvir <tanvir.ahmed.tonoy@proton.me>
2023-08-15 21:13:04 +0600

    add alias for pavucontrol and blueman
    Signed-off-by: tanvir.ahmed.tonoy@proton.me

--- common/alias.lua
+++ common/alias.lua
@@ -6,5 +6,7 @@ local appnames = {}
 
 appnames["firefox"] = "FF"
 appnames["Code"] = "VSCode"
+appnames["Pavucontrol"] = "PavuCon"
+appnames[".blueman-manager-wrapped"] = "BlueMan"
 
 return appnames
