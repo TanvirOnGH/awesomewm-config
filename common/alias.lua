383fde937ea9383187fdd973e5d1faa1d94e94df
Tanvir <tanvir.ahmed.tonoy@proton.me>
2023-08-18 16:17:15 +0600

    add alias for libreworlf, keepassxc and qbittorrent
    Signed-off-by: tanvir.ahmed.tonoy@proton.me

--- common/alias.lua
+++ common/alias.lua
@@ -4,9 +4,13 @@
 
 local appnames = {}
 
+-- Find exact names (WM_CLASS(STRING)) using `xprop`
 appnames["firefox"] = "FF"
+appnames["librewolf"] = "L-Wolf"
 appnames["Code"] = "VSCode"
 appnames["Pavucontrol"] = "PavuCon"
 appnames[".blueman-manager-wrapped"] = "BlueMan"
+appnames["KeePassXC"] = "KeePass"
+appnames["qBittorrent"] = "QBit"
 
 return appnames
