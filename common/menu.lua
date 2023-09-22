9e2655a85f977a39bf30f684cb481b799b118be5
TanvirOnGH <TanvirOnGH@users.noreply.github.com>
2023-09-22 18:18:14 +0000

    chore: linting with Stylua

--- common/menu.lua
+++ common/menu.lua
@@ -83,7 +83,7 @@ function menu:init(args)
 			{ "Thunar", env.fm, micon("folder") },
 			{ "Firefox", "firefox", micon("firefox") },
 			{ "VSCode", "code", micon("code-editor") },
-            separator,
+			separator,
 			{ "Lock Screen", "i3lock-fancy-rapid 5 5", micon("exit") },
 			separator,
 			{ "Exit", exitmenu, micon("exit") },
