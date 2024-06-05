be020b8fd18ef0e2c5db2f5dd63c30c5b7bb259c
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-05 15:53:27 +0600

    replace nmtui to nm-tray in autostart

--- common/autostart.lua
+++ common/autostart.lua
@@ -15,7 +15,7 @@ function autostart.run()
 	-- utils
 	awful.spawn.with_shell("picom --daemon") -- compositor
 	awful.spawn.with_shell("copyq") -- clipboard manager daemon
-	awful.spawn.with_shell("nmtui") -- NetworkManager Tray
+	awful.spawn.with_shell("nm-tray") -- NetworkManager Tray
 	-- awful.spawn.with_shell("flashfocus") -- flashfocus daemon
 
 	-- apps
