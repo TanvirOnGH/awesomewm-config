7b46f0d0a4d502afebff433edf2af5d1f6d96264
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-23 15:01:50 +0600

    add dwindle layout
    Signed-off-by: tanvir.ahmed.tonoy@proton.me

--- common/layout.lua
+++ common/layout.lua
@@ -12,17 +12,18 @@ function layouts:init()
 	local layset = {
 		awful.layout.suit.floating,
 		flex.layout.map,
-		awful.layout.suit.tile,
+		awful.layout.suit.tile, -- Right
 		awful.layout.suit.tile.left,
 		awful.layout.suit.tile.bottom,
 		awful.layout.suit.tile.top,
-		awful.layout.suit.fair,
+		awful.layout.suit.fair, -- Vertical
 		awful.layout.suit.fair.horizontal,
 		awful.layout.suit.corner.nw,
 		awful.layout.suit.corner.ne,
 		awful.layout.suit.corner.sw,
 		awful.layout.suit.corner.se,
 		awful.layout.suit.spiral,
+		awful.layout.suit.spiral.dwindle,
 		awful.layout.suit.magnifier,
 		flex.layout.grid,
 		awful.layout.suit.max.fullscreen,
