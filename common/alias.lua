65963c6ffc65759a94e0c964e5f9ec0864f05681
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-06-26 15:26:11 +0600

    add more appname alias entries
    Signed-off-by: tanvir.ahmed.tonoy@proton.me

--- common/alias.lua
+++ common/alias.lua
@@ -1,16 +1,29 @@
 local appnames = {}
 
+-- All names in the bar appears in capital case
 -- Find exact names (WM_CLASS(STRING)) using `xprop`
-appnames["firefox"] = "FF"
-appnames["thunderbird"] = "TB"
-appnames["betterbird"] = "BB"
-appnames["librewolf"] = "L-Wolf"
+-- xprop | grep WM_CLASS
+-- For multiple names, use the last one
+appnames["kitty"] = "Kitty"
+appnames["firefox"] = "Firefox"
+appnames["thunderbird"] = "Thunderbird"
+appnames["betterbird"] = "Betterbird"
+appnames["librewolf"] = "LibreWolf"
 appnames["Code"] = "VSCode"
-appnames["Pavucontrol"] = "PavuCon"
+appnames["Pavucontrol"] = "PavuControl"
 appnames[".blueman-manager-wrapped"] = "BlueMan"
-appnames["KeePassXC"] = "KeePass"
-appnames["qBittorrent"] = "QBit"
+appnames["KeePassXC"] = "KeePassXC"
+appnames["qBittorrent"] = "qBittorrent"
 appnames["revolt-desktop"] = "Revolt"
 appnames["wihotspot-gui"] = "Hotspot"
+appnames["Tribler"] = "Tribler"
+appnames["Tor Browser"] = "Tor"
+appnames["Pineapple Pictures"] = "PineAppl"
+appnames["thunar"] = "Thunar"
+appnames["Cozy Drive"] = "Cozy"
+appnames["chat-simplex-desktop-MainKt"] = "Simplex"
+appnames["org.gnome.Boxes"] = "Boxes"
+appnames["GitKraken"] = "GKraken"
+appnames["GitHub Desktop"] = "GH Desk"
 
 return appnames
