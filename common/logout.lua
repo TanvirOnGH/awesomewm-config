2f33c7da11cd1d80ccaa89f8d8bb95e42c3f43e1
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-07-02 20:06:58 +0600

    add lockscreen to logout entries

--- common/logout.lua
+++ common/logout.lua
@@ -13,6 +13,14 @@ function logout:init()
 			label = "Logout",
 			close_apps = true,
 		},
+		{ -- Lock screen
+			callback = function()
+				awful.spawn.with_shell("sleep 1 && i3lock-fancy-rapid 5 5")
+			end,
+			icon_name = "lock",
+			label = "Lock",
+			close_apps = false,
+		},
 		{ -- Shutdown
 			callback = function()
 				awful.spawn.with_shell("systemctl poweroff")
