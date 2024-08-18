3900210dd549da9b4754b6eefa6b39d57a7d2ab5
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-08-18 17:19:57 +0600

    change refresh rate to 100 (xrandr) and update flex submodule

--- common/autostart.lua
+++ common/autostart.lua
@@ -7,7 +7,7 @@ local autostart = {}
 -- Application list function
 function autostart.run()
 	-- essentials
-	awful.spawn.with_shell("xrandr --output HDMI-0 --primary --mode 1920x1080 --rate 75 --pos 0x0 --rotate normal") -- display output
+	awful.spawn.with_shell("xrandr --output HDMI-0 --primary --mode 1920x1080 --rate 100 --pos 0x0 --rotate normal") -- display output
 	awful.spawn.with_shell("xset -dpms") -- prevent X server from turning off the display
 	awful.spawn.with_shell("xset s off") -- disable X screen saver feature
 	awful.spawn.with_shell("xset r rate 250 50") -- speed up keyboard auto-repeat rate
