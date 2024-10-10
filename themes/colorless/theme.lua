3322d463c80a63d41ec9631332e16ccce126d38e
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-10-10 18:01:24 +0000

    format lua code with stylua

--- themes/colorless/theme.lua
+++ themes/colorless/theme.lua
@@ -53,8 +53,16 @@ helpers.shuffle_table(wallpapers)
 
 -- Assign unique wallpapers to tags, reuse if not enough
 local tags = {
-	"1:1", "1:2", "1:3", "1:4", "1:5",
-	"2:1", "2:2", "2:3", "2:4", "2:5"
+	"1:1",
+	"1:2",
+	"1:3",
+	"1:4",
+	"1:5",
+	"2:1",
+	"2:2",
+	"2:3",
+	"2:4",
+	"2:5",
 }
 
 theme.wallpapers = {}
